db26a04c8e756134548438cecc6ca9fd5d79d719
swayamvikhepatil <swayamvikhe07@gmail.com>
2026-07-29 10:09:34 +0530

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,14 @@
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling function: {func.__name__}")
+        result = func(*args, **kwargs)
+        print(f"Function {func.__name__} executed successfully.")
+        return result
+    return wrapper
+
+@log_function_call
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(2, 3)
+print(result)
